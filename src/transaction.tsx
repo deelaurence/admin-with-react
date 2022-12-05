9ab9cca084c8d63ad94babb4ed9d7bf001431b22
deelaurence <laurie143pelumi@gmail.com>
2022-12-05 14:36:14 +0100

    first commit

---
+++ src/transaction.tsx
@@ -0,0 +1,95 @@
+import { useMediaQuery } from "@mui/material";
+import {
+    List,
+    Datagrid,
+    TextField,
+    SimpleList,
+    EmailField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    ReferenceInput,
+    TextInput,
+} from "react-admin";
+// import { List, Datagrid, TextField, ReferenceField, EditButton } from "react-admin"
+import { Link } from "@mui/material";
+import LaunchIcon from "@mui/icons-material/Launch";
+import React from "react";
+import { useEffect, useState } from "react";
+export const TransactionList = () => {
+    const [dimensions, setDimensions] = React.useState({
+        width: window.innerWidth
+    })
+    const [isSmall, setIsSmall] = React.useState(false)
+    React.useEffect(() => {
+        function handleResize() {
+            setDimensions({ width: window.innerWidth })
+            if (window.innerWidth < 400) {
+                setIsSmall(true)
+            }
+        }
+        window.addEventListener('resize', handleResize)
+    })
+    const postFilters = [
+        <TextInput source="q" label="Search" alwaysOn />,
+        <ReferenceInput source="userId" label="User" reference="users" />,
+    ];
+
+    // export const PostList = () => (
+    //     <List filters={postFilters}>
+    //     // ...
+    //     </List>
+    // );
+
+    return (
+
+        <List filters={postFilters}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.owner.email}
+                    secondaryText={(record) => record.amount}
+                    tertiaryText={(record) => record.status}
+                />
+            ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="owner.name" />
+                    <TextField source="amount" />
+                    <TextField source="status" />
+                    <EmailField source="owner.email" />
+                </Datagrid>
+            )}
+
+        </List>
+    );
+};
+export const TransactionEdit = () => {
+    const [dimensions, setDimensions] = React.useState({
+        width: window.innerWidth
+    })
+    const [isSmall, setIsSmall] = React.useState(false)
+    React.useEffect(() => {
+        function handleResize() {
+            setDimensions({ width: window.innerWidth })
+            if (window.innerWidth < 400) {
+                setIsSmall(true)
+            }
+        }
+        window.addEventListener('resize', handleResize)
+    })
+    return (
+
+        <Edit>
+            <SimpleForm>
+                {/* <ReferenceInput source="transaction.status" reference="transactions" /> */}
+                <TextInput source="status" />
+                <p>Spelling must match with either (pending-failed-approved)</p>
+                <TextInput disabled source="amount" />
+                <TextInput disabled source="owner.name" />
+                <TextInput disabled source="owner.pendBalance" />
+            </SimpleForm>
+
+
+        </Edit>
+    );
+};
---
+++ src/user.tsx
@@ -0,0 +1,41 @@
+import { useMediaQuery } from "@mui/material";
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import { Link } from "@mui/material";
+import LaunchIcon from "@mui/icons-material/Launch";
+import React from "react";
+import { useEffect, useState } from "react";
+import MyNameField from "./MyNameField";
+export const UserList = () => {
+    const [dimensions, setDimensions] = React.useState({
+        width: window.innerWidth
+    })
+    const [isSmall, setIsSmall] = React.useState(false)
+    React.useEffect(() => {
+        function handleResize() {
+            setDimensions({ width: window.innerWidth })
+            if (window.innerWidth < 400) {
+                setIsSmall(true)
+            }
+        }
+        window.addEventListener('resize', handleResize)
+    })
+    return (
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.name}
+                    secondaryText={(record) => record.zipCode}
+                    tertiaryText={(record) => record.email}
+                />
+            ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <MyNameField source="name" />
+                    <TextField source="zipCode" />
+                    <EmailField source="email" />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
